ee7b0bfb95f62faa35e8cfb9b6a30d6daccc6742
Abhishek97Ojha <abhishek97ojha@gmail.com>
2023-04-26 00:30:57 +0530

    Starred

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Home from "./components/Home";
+// import { plainCallApi } from "./plainredux/actions";
+import { githubAction } from "./redux/reducers";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state);
+  console.log("selector in app.js", selector);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="nav">
+        <h1>Most Starred Repos</h1>
+      </div>
+      <div className="home">
+        <Home />
+      </div>
     </div>
   );
 }
